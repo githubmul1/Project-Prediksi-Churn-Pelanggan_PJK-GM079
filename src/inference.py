8616c5cf93b6294911138239265daa061945ef64
wandrie <evandrie13@gmail.com>
2026-05-12 20:39:50 +0800

    mengembalikan inference setelah konflik

--- src/inference.py
+++ src/inference.py
@@ -1,12 +1,7 @@
 import joblib
 import pandas as pd
-<<<<<<< HEAD
 import shap
 
-from datetime import datetime
-
-from config import MODEL_PATH
-=======
 from datetime import datetime
 
 try:
@@ -15,7 +10,6 @@ except ImportError:
     shap = None
 
 from config import MODEL_PATH, PIPELINE_PATH
->>>>>>> 61dbf02 (perubahan sebelum pull)
 from src.explainer import explain_churn, generate_explanation
 
 # =========================
@@ -23,18 +17,7 @@ from src.explainer import explain_churn, generate_explanation
 # =========================
 model = joblib.load(MODEL_PATH)
 
-<<<<<<< HEAD
-# ambil bagian preprocessing
-preprocessor = model.named_steps["preprocessor"]
-
-# ambil random forest model
-rf_model = model.named_steps["model"]
-
-# SHAP explainer
-explainer = shap.Explainer(rf_model)
-=======
 explainer = shap.Explainer(model) if shap is not None else None
->>>>>>> 61dbf02 (perubahan sebelum pull)
 
 
 # =========================
